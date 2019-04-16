580957f112edd3ee62ee7c68d193c8c58d063e4c
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2019-04-16 17:59:58 -0300

    Show books from database

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,12 +1,16 @@
 require 'features_helper'
 
 RSpec.describe 'List books' do
-  it 'displays each book on the page' do
-    repository = BookRepository.new
+  let(:repository)  { BookRepository.new }
+
+  before do
+    repository.clear
 
     repository.create(title: 'PoEAA', author: 'Martin Fowler')
     repository.create(title: 'TDD',   author: 'Kent Beck')
+  end
 
+  it 'displays each book on the page' do
     visit '/books'
 
     within '#books' do
