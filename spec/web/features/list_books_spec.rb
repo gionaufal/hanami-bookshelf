52087bf6b5718dd75dce42e96b93360fc3b65e1c
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2019-04-16 17:36:33 -0300

    Create books

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -2,10 +2,15 @@ require 'features_helper'
 
 RSpec.describe 'List books' do
   it 'displays each book on the page' do
+    repository = BookRepository.new
+
+    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+    repository.create(title: 'TDD',   author: 'Kent Beck')
+
     visit '/books'
 
     within '#books' do
-      expect(page).to have_css('.book', count: 2)
+      expect(page).to have_selector('.book', count: 2), 'Expected to find 2 books'
     end
   end
 end
